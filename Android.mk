7d166a7859fbcc371df6383da0957f8d4c51d4d5
辛道韦 <151035881+xindaowei0@users.noreply.github.com>
2024-08-11 00:15:32 +0800

    add
    该项目为首次上传

--- Android.mk
+++ Android.mk
@@ -1,10 +1,12 @@
-# Android device tree for Xiaomi Pad 6S Pro 12.4 (sheng)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),sheng)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,14 @@
-# Android device tree for Xiaomi Pad 6S Pro 12.4 (sheng)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_sheng.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_sheng-user \
+    omni_sheng-userdebug \
+    omni_sheng-eng
